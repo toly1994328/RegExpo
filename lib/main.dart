e1bad9f3babdaa85dd1cffe134571ce7c4fe1000
toly <1981462002@qq.com>
2022-12-15 11:47:29 +0800

    。

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,33 @@ import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:regexpo/src/blocs/bloc_wrapper.dart';
+import 'package:window_manager/window_manager.dart';
 import 'src/blocs/bloc_relation.dart';
 
 import 'src/views/desk_ui/splash/splash_page.dart';
 
-void main() {
-  runApp(const BlocWrapper(child:  MyApp()));
+void main() async{
   if(Platform.isMacOS||Platform.isWindows||Platform.isLinux){
-    DesktopWindow.setWindowSize(const Size(900, 600));
-    DesktopWindow.setMinWindowSize(const Size(600, 400));
+    // DesktopWindow.setWindowSize(const Size(900, 600));
+    // DesktopWindow.setMinWindowSize(const Size(600, 400));
+    WidgetsFlutterBinding.ensureInitialized();
+    // Must add this line.
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = WindowOptions(
+      size: Size(900, 600),
+      center: true,
+      // backgroundColor: Colors.transparent,
+      // skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+
+    runApp(const BlocWrapper(child:  MyApp()));
+
   }
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,16 @@ import Foundation
 
 import desktop_window
 import path_provider_macos
+import screen_retriever
 import shared_preferences_macos
 import sqflite
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
